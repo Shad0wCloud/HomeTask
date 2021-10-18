da7293d4d60e2dbc5915cf05f3283fd906bb6269
Shad0wCloud <miklking9@gmail.com>
2021-10-19 02:09:12 +0300

    Вторая домаха

---
+++ исходники/Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class Test_Class
+    {
+
+
+        private Dictionary<string, int>UniqueWords(string[] input)
+        {
+            Dictionary<string, int> unique = new Dictionary<string, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                input[i] = Regex.Replace(input[i], "[.?!)(,:…«»;„“№]", "");//избавимся от знаков припинания
+                if (!unique.ContainsKey(input[i].ToLower()))
+                {
+                    unique.Add(input[i].ToLower(), 1); //если еще такого слова нет в словаре, то мы создадим пару ключ + значение
+                }
+                else
+                {
+                    //достаем значение ключа по самому ключу и ув. его на 1
+                    int cur_count;
+                    unique.TryGetValue(input[i].ToLower(), out cur_count);
+                    cur_count++;
+                    unique[input[i].ToLower()] = cur_count;
+                }
+            }
+            return unique;
+        }
+    }
+}
---
+++ исходники/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using MyLibrary;
+
+namespace unique_words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path_file = "idiot.txt";
+            string path_result = "result.txt";
+            StreamReader sr = new StreamReader(path_file);
+            string[] text_by_words = sr.ReadToEnd().Split(new char[] { ' ', '—', '–', '-', '\n' }); //разделим строки 
+            sr.Close();
+            text_by_words = text_by_words.Where(x => !string.IsNullOrEmpty(x)).ToArray(); // избавимся от пустых элементов массива
+            //начало reflection
+            Test_Class inst = new Test_Class();
+            var meth = inst.GetType().GetMethod("UniqueWords", BindingFlags.NonPublic | BindingFlags.Instance);
+            
+            var returnValue = (Dictionary<string, int>) meth.Invoke(inst, new object[] { text_by_words });
+            List<KeyValuePair<string, int>> convert = returnValue.ToList();
+            convert.Sort((pair1, pair2) => pair2.Value.CompareTo(pair1.Value)); //сортировка по убыванию
+            StreamWriter sw = new StreamWriter(path_result, false, System.Text.Encoding.Default);
+            foreach (var kv in convert)
+            {
+                sw.WriteLine($"{kv.Key} - {Convert.ToString(kv.Value)}");
+            }
+            sw.Close();
+            Console.WriteLine("Done!");
+        }
+    }
+}
